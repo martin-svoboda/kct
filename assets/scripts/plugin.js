e4b060269bb179b67a16057cf4d8e33696f91c07
Martin Svoboda <ja@martin-svoboda.cz>
2024-09-09 14:54:05 +0200

    WIP

--- assets/scripts/plugin.js
+++ assets/scripts/plugin.js
@@ -1,7 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
 	var obfuscatedEmails = document.querySelectorAll('.email-obfuscated');
+
 	obfuscatedEmails.forEach(function(element) {
 		var obfuscatedEmail = element.getAttribute('data-email').replace(" [zav] ", "@");
 		element.innerHTML = '<a href="mailto:' + obfuscatedEmail + '">' + obfuscatedEmail + '</a>';
+	});
+
+	const toggleButton = document.querySelector('.menu-toggle');
+	const menu = document.getElementById('primary-menu');
+
+	toggleButton.addEventListener('click', function() {
+		const isExpanded = toggleButton.getAttribute('aria-expanded') === 'true';
+
+		// Přepnutí atributu aria-expanded
+		toggleButton.setAttribute('aria-expanded', !isExpanded);
+
+		// Přepnutí zobrazení menu
+		if (!isExpanded) {
+			menu.style.display = 'block';
+		} else {
+			menu.style.display = '';
+		}
 	});
 });
--- themes/kct/header.php
+++ themes/kct/header.php
@@ -63,13 +63,17 @@
 
 				<nav id="site-navigation" class="main-navigation">
 					<button class="menu-toggle" aria-controls="primary-menu"
-							aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'kct' ); ?></button>
+							aria-expanded="false">
+						<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+							<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+								  stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+						</svg> <span><?php esc_html_e( 'Menu', 'kct' ); ?></span></button>
 					<?php
 					wp_nav_menu(
-							array(
-									'theme_location' => 'menu-1',
-									'menu_id'        => 'primary-menu',
-							)
+						array(
+							'theme_location' => 'menu-1',
+							'menu_id'        => 'primary-menu',
+						)
 					);
 					?>
 				</nav><!-- #site-navigation -->
